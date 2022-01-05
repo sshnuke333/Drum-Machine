6919f24b4791ddf6b81e70ea0c5650535bc19d61
sshnuke333 <sshnuke@protonmail.com>
2022-01-06 01:10:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Drum Machine
+
+Part of FreeCodeCamp's Front End Development Libraries Project challenge built
+using React and styled-components
---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+
+export default function App() {
+    return <div id="drum-machine"></div>;
+}
