213c9a15fc60be253e3906ef9768c4ff4ded1129
sshnuke333 <sshnuke@protonmail.com>
2022-01-17 22:10:23 +0530

    added styles

---
+++ src/App.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { Card, Box } from '@mui/material';
+
+export const StyledCard = styled(Card)`
+    background-color: #1e1e1e;
+    color: white;
+    height: auto;
+    display: flex;
+    flex-flow: column;
+`;
+
+export const StyledBox = styled(Box)`
+    display: grid;
+    height: 50rem;
+    width: 40rem;
+    grid-template-columns: 2fr 2fr 2fr;
+    grid-template-rows: 0.5fr 2fr 2fr 2fr 0.5fr;
+    align-items: center;
+    justify-items: center;
+`;
